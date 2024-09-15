8c0ca2da33040085200659501b35db4d0732e984
Black foxi <dentef767@gmail.com>
2024-09-15 17:31:33 +0100

    first commit

---
+++ Dto/Models/Countries/create-country-dto.ts
@@ -0,0 +1,12 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+import { z } from 'zod';
+
+export const createCountryDtoSchema = z.object({
+    name: z.string().length(128),
+    icon: z.string().length(2048)
+});
+
+export type CreateCountryDto = z.infer<typeof createCountryDtoSchema>;
---
+++ Dto/Models/Countries/update-country-dto.ts
@@ -0,0 +1,13 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+import { z } from 'zod';
+
+export const updateCountryDtoSchema = z.object({
+    name: z.string().length(128),
+    icon: z.string().length(2048),
+    id: z.string().uuid()
+});
+
+export type UpdateCountryDto = z.infer<typeof updateCountryDtoSchema>;
